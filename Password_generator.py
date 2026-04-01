fbc88bb069d107b9f83633148220fac04e02efa9
praveen135333 <159537399+praveen135333@users.noreply.github.com>
2026-04-01 16:46:58 +0530

    Update Password_generator.py

--- Password_generator.py
+++ Password_generator.py
@@ -10,14 +10,17 @@ def passwordGenerator(length=8, use_upper=True, use_lower=True, use_numbers=True
     if use_numbers:
         characters += string.digits
     if use_special:
-        characters += string.punctuation
+        # characters += string.punctuation
+        characters += '@!?#&*$=/'
     if characters == '':
         print('Error: No character types selected.')
         return 
     password = ''.join(random.choice(characters) for i in range(length))
     return password
 copy_of_password = passwordGenerator(12)
-passwordList = list(copy_of_password)
+# passwordList = list(copy_of_password)
+
 with open("password.txt", "w") as f:
-    f.write(str(passwordList))
+    # f.write(str(passwordList))
+    f.write(copy_of_password+"\n")
 print(copy_of_password)
