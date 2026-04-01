936f5a7c2f3cec15ada7ae1628059c485c469524
praveen135333 <159537399+praveen135333@users.noreply.github.com>
2026-04-01 16:46:49 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,27 +1,29 @@
 def calculator():
     var1 = int(input("Enter the first variable: "))
     var2 = int(input("Enter the second variable: "))
-    while True: 
-        print("------->Enter operation you want to perform:\n1.Add\n2.Subtract\n3.Multiply\n4.Division\n5.Exit")
-        operation = int(input("Enter the choice: "))
-        if operation == 1:
-            result = var1+var2
-            print(f"Addition of {var1} and {var2} is {result}.")
-        elif operation == 2:
-            if var1>=var2:
-                result = var1-var2
-            else:
-                result = var2 - var1
-            print(f"Difference of {var1} and {var2} is {result}.")
-        elif operation == 3:
-            result = var1*var2
-            print(f"Multiplication of {var1} and {var2} is {result}.")
-        elif operation == 4:
-            result = var1/var2
-            print(f"Division of {var1} and {var2} is {result}.")
-        elif operation == 5:
-            print("thanks for using calculator")
-            return
+    print("------->Enter operation you want to perform:\n1.Add\n2.Subtract\n3.Multiply\n4.Division\n5.Power\n5.Exit")
+    operation = int(input("Enter the choice: "))
+    if operation == 1:
+        result = var1+var2
+        print(f"Addition of {var1} and {var2} is {result}.")
+    elif operation == 2:
+        if var1>=var2:
+            result = var1-var2
         else:
-            print("wrong choice.. Please Start Again.")
+            result = var2 - var1
+        print(f"Difference of {var1} and {var2} is {result}.")
+    elif operation == 3:
+        result = var1*var2
+        print(f"Multiplication of {var1} and {var2} is {result}.")
+    elif operation == 4:
+        result = var1/var2
+        print(f"Division of {var1} and {var2} is {result}.")
+    elif operation == 5:
+        result = var1**var2
+        print(f"Division of {var1} and {var2} is {result}.")
+    elif operation == 5:
+        print("thanks for using calculator")
+        return
+    else:
+        print("wrong choice.. Please Start Again.")
 calculator()
